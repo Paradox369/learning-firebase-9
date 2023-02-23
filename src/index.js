6e1423979201bb2367587a5212f06e3ae81c6be6
Paradox369 <george.ketcha@gmail.com>
2023-02-23 04:34:02 +0100

    updating data in real time

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUwIZ5rj2z9_ErYQdyjZ2ltTVALAdc3tw",
+  authDomain: "paradox-learns-firebase-9.firebaseapp.com",
+  projectId: "paradox-learns-firebase-9",
+  storageBucket: "paradox-learns-firebase-9.appspot.com",
+  messagingSenderId: "210108796849",
+  appId: "1:210108796849:web:d7a302fd43afb78828b790",
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, "books");
+
+// get collection data
+onSnapshot(colRef, (snapshot) => {
+  let books = [];
+
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+
+  console.log(books);
+});
+
+// add docs
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  }).then(() => addBookForm.reset());
+});
+
+// delete docs
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const docRef = doc(db, "books", deleteBookForm.id.value);
+  deleteDoc(docRef).then(() => deleteBookForm.reset());
+});
